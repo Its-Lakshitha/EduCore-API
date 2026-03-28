a803bd92230c48047d0f4e7d8cfd782656f9f390
Lakshitha Fernando <avishkalakshitha0502@gmail.com>
2026-03-29 00:52:02 +0530

    Enable SSL redirect and secure cookies
    Add production-only security settings to enforce HTTPS and secure cookies when DEBUG is False. Sets SECURE_SSL_REDIRECT, SESSION_COOKIE_SECURE, and CSRF_COOKIE_SECURE in settings.py; compiled settings bytecode (.pyc) was also updated.

--- student_management/settings.py
+++ student_management/settings.py
@@ -132,6 +132,11 @@ USE_I18N = True
 
 USE_TZ = True
 
+if not DEBUG:
+    SECURE_SSL_REDIRECT = True
+    SESSION_COOKIE_SECURE = True
+    CSRF_COOKIE_SECURE = True
+
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
