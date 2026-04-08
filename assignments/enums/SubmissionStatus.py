de8efd2deaff40a4a7f7ca8d379e35539f1f3d01
Lakshitha Fernando <avishkalakshitha0502@gmail.com>
2026-04-08 13:42:44 +0530

    Improvements / Add new choices to SubmissionStatus
    For perform the smooth functionality of the late submission process and deadline extension, needed to add new two choices to SubmissionStatus.py of enums dir. This choices describe submitted extension for globally as SUBMITTED_ON_EXTENSION and submitted extension on special / individual cases as  SUBMITTED_ON_SPECIAL_EXTENSION

--- assignments/enums/SubmissionStatus.py
+++ assignments/enums/SubmissionStatus.py
@@ -5,4 +5,6 @@ class SubmissionStatus(models.TextChoices):
     PENDING = 'PENDING', 'Pending'
     SUBMITTED = 'SUBMITTED', 'Submitted'
     LATE = 'LATE', 'Late'
+    SUBMITTED_ON_SPECIAL_EXTENSION = 'SUBMITTED_ON_SPECIAL_EXTENSION', 'Submitted on Special Extension',
+    SUBMITTED_ON_EXTENSION = 'SUBMITTED_ON_EXTENSION', 'Submitted on Extension',
     GRADED = 'GRADED', 'Graded'
